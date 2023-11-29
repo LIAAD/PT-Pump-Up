a7e09c32a3fd31eec6397e1883421c141bfec297
Rúben Almeida <almeidaruben35@gmail.com>
2023-11-29 17:10:58 +0000

    Fixed Responsiveness in the homepage

--- src/frontend/src/assets/admin/HeaderAdmin.js
+++ src/frontend/src/assets/admin/HeaderAdmin.js
@@ -4,18 +4,19 @@ import inescLogo from '../images/INESCTEC.png'
 import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
 
 
 const HeaderAdmin = (props) => {
     return (
         <Grid container alignItems="center">
-            <Grid item xs={"auto"} sx={{ mr: 2 }}>
+            <Grid item md={"auto"} sx={{ mr: 2 }}>
                 <Button href="/" sx={{ p: 0 }}><img id={"logo"} src={inescLogo} /></Button>
             </Grid>
-            <Grid item xs={"auto"} lg={6}>
-                <h1><span className="blue-inesc">PT-Pump-Up:</span> <span id="small-title">Hub for Portuguese NLP Resources</span></h1>
+            <Grid item md={"auto"} lg={6}>
+                <h1><span className="blue-inesc">PT-Pump-Up</span> <Box id="small-title" sx={{ display: { xs: 'none', md: 'inline' } }}>Hub for Portuguese NLP Resources</Box></h1>
             </Grid>
-            <Grid item xs={"auto"} sx={{ ml: "auto" }}>
+            <Grid item md={1} sx={{ ml: "auto" }}>
                 <IconButton className="blue-inesc" onClick={props.toggleDrawer} sx={{ p: 2 }} size="large"><MenuIcon fontSize="inherit" /></IconButton>
             </Grid>
         </Grid>
