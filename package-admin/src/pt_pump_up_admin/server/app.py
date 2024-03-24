cbec02bd27afe2c7ef5dd4f296f1dd0174fc07c4
Rúben Almeida <almeidaruben35@gmail.com>
2024-03-24 12:01:02 +0000

    Establishing foundations for Microservices Architecture in Admin Package

--- package-admin/src/pt_pump_up_admin/server/app.py
+++ package-admin/src/pt_pump_up_admin/server/app.py
@@ -3,25 +3,41 @@ from flask_cors import CORS
 from pt_pump_up_orms.orms import Dataset
 import requests
 from pt_pump_up_admin.integration import P
+from datasets import load_dataset
+from transformers import pipeline
 
 app = Flask(__name__)
 CORS(app)
 
-# Parse Json request for train decision tree
 
-# Parse Json request for inference decision tree
+def _extract_varieties_stats(varieties):
+    raise NotImplementedError
+    model = pipeline('text-classification', model="")
+    dataset_dict = load_dataset(dataset.link.hugging_face_url.split('/')[-1])
 
-# Parse Json request for papers with code insert paper
+    for split in ['train', 'validation', test]:
+        for text in dataset_dict[split]:
+            model(text)
 
-# Parse Json request for papers with code insert dataset
 
 @app.route('/papers_with_code/insert_dataset', methods=['POST'])
 def event_insert_dataset():
+    raise NotImplementedError
+
     dataset = Dataset(**request.json)
 
-    # Push Dataset to PapersWithCode
-    
-    
+    if dataset.link.papers_with_code_url:
+        # Push Dataset to PapersWithCode
+        raise NotImplementedError
+
+    if dataset.link.hugging_face_url:
+        varieties = _extract_varieties_stats(dataset.link.hugging_face_url)
+
+    # Extract Language Variety Stats
+    varieties = None
 
 
 # Parse Json request for crawl websites
+@app.route('/fetch_resources', methods=['POST'])
+def event_fetch_resources():
+    raise NotImplementedError
